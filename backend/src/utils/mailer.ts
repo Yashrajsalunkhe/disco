fef7bf6e56992d843a78e2a3f6ca4adeb148e277
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-09-21 17:22:04 +0530

    updated server
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -2,6 +2,10 @@ import nodemailer from "nodemailer";
 import { Transporter } from "nodemailer";
 import path from 'path';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
